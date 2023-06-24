dffe0722dce0d2042fb939a9e71d8bbc975091d9
kotfind <kotfind@yandex.ru>
2023-06-24 13:28:43 +0300

    Delete dummy nodes and arrows

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -8,22 +8,4 @@
 
 Scene::Scene(QObject* parent)
   : QGraphicsScene(parent)
-{
-    constexpr int n = 5;
-    Node* nodes[n];
-    for (int i = 0; i < 5; ++i) {
-        auto* n = nodes[i] = new Node;
-        n->setPlainText(QString("Node %1").arg(i + 1));
-        n->setPos(0, i * 100);
-        addItem(n);
-    }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            auto* a = new Arrow(nodes[i], nodes[j]);
-            addItem(a);
-        }
-    }
-
-    update();
-}
+{}
